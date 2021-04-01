139472b7f4f4281373372776cf3c4b4fc09e3bfb
Prajwal T S <prajwal@higin.in>
2021-04-02 02:36:29 +0530

    dev build fix

--- build.ts
+++ build.ts
@@ -2,9 +2,10 @@
  * Remove old files, copy front-end ones.
  */
 
+import childProcess from 'child_process';
+import commandLineArgs from 'command-line-args';
 import fs from 'fs-extra';
 import Logger from 'jet-logger';
-import childProcess from 'child_process';
 
 // Setup logger
 const logger = new Logger();
@@ -15,10 +16,18 @@ logger.timestamp = false;
 
 (async () => {
     try {
+        const options = commandLineArgs([
+            {
+                name: 'env',
+                alias: 'e',
+                defaultValue: 'local',
+                type: String,
+            },
+        ]);
         // Remove current build
         await remove('./dist/');
         // Copy production env file
-        await copy('./src/pre-start/env/production.env', './dist/pre-start/env/production.env');
+        await copy(`./src/pre-start/env/${options.env}.env`, `./dist/pre-start/env/${options.env}.env`);
         // Copy back-end files
         await exec('tsc --build tsconfig.prod.json', './')
     } catch (err) {
@@ -47,7 +56,7 @@ function copy(src: string, dest: string): Promise<void> {
 
 function exec(cmd: string, loc: string): Promise<void> {
     return new Promise((res, rej) => {
-        return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
+        return childProcess.exec(cmd, { cwd: loc }, (err, stdout, stderr) => {
             if (!!stdout) {
                 logger.info(stdout);
             }
