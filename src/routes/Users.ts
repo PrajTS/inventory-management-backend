692a1392e14959e27766d7dafef52176b62908eb
Prajwal T S <prajwal@higin.in>
2021-04-23 02:22:04 +0530

    serialize body for proxy

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,5 +1,5 @@
 import { authProxy } from '@middlewares/Proxy';
-import { Router } from 'express';
+import { Request, Router } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const router = Router();
@@ -7,7 +7,15 @@ const router = Router();
 router.use('/', authProxy,
     createProxyMiddleware({
         target: process.env.USER_MANAGER_MICROSERVICE_URL,
-        changeOrigin: true
+        changeOrigin: true,
+        onProxyReq: (proxyReq, req: Request) => {
+            if (req?.body) {
+                const bodyData = JSON.stringify(req.body);
+                proxyReq.setHeader('Content-Type', 'application/json');
+                proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+                proxyReq.write(bodyData);
+            }
+        }
     })
 );
 
