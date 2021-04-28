e42313bbacbc3532988325eca1b3b12027710dd9
Prajwal T S <prajwal@higin.in>
2021-04-28 12:44:33 +0530

    add team and tournaments proxy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import AuthRouter from './Auth';
 import UsersRouter from './Users';
+import TeamsRouter from './Teams';
 
 // Init router and path
 const router = Router();
@@ -11,6 +12,8 @@ router.get('/ping', (req: Request, res: Response) => res.send("pong"));
 router.use('/v1/auth', AuthRouter);
 
 router.use('/v1/account', UsersRouter);
+
+router.use('/v1/teams', TeamsRouter);
 
 // Export the base-router
 export default router;
