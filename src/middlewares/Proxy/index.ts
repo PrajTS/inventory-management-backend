85d6833040f129c0d8e35921f86486cd692b3120
Prajwal T S <prajwal@higin.in>
2021-05-03 17:08:00 +0530

    Remove hardcoded

--- src/middlewares/Proxy/index.ts
+++ src/middlewares/Proxy/index.ts
@@ -3,11 +3,9 @@ import { NextFunction, Request, Response } from "express";
 export const authProxy = (request: Request, response: Response, next: NextFunction) => {
     const initialValue = request.headers.authorization;
     try {
-        request.headers.authorization = JSON.stringify({id: "608952798ee39b75df7f12ed"});
-        // TODO Uncomment 8-10 and remove line 6
-        // if (request.session.user) {
-        //     request.headers.authorization = JSON.stringify(request.session.user);
-        // }
+        if (request.session.user) {
+            request.headers.authorization = JSON.stringify(request.session.user);
+        }
     } catch (e) {
         request.headers.authorization = initialValue;
     }
