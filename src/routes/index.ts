c1b420c508db9ccb390300af8acfb6e76b036524
Prajwal T S <prajwal@higin.in>
2021-05-04 23:15:55 +0530

    Add tournaments route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,25 @@
-import { Request, Response, Router } from 'express';
-import AuthRouter from './Auth';
-import UsersRouter from './Users';
-import TeamsRouter from './Teams';
+import { Request, Response, Router } from "express";
+import AuthRouter from "./Auth";
+import TeamsRouter from "./Teams";
+import UsersRouter from "./Users";
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.get('/ping', (req: Request, res: Response) => res.send("pong"));
+router.get("/ping", (req: Request, res: Response) => res.send("pong"));
 
-router.use('/v1/auth', AuthRouter);
+router.use("/v1/auth", AuthRouter);
 
-router.use('/v1/account', UsersRouter);
+router.use("/v1/account", UsersRouter);
 
-router.use('/v1/teams', TeamsRouter);
+router.use("/v1/teams", TeamsRouter);
 
-router.use('/v1/games', TeamsRouter);
+router.use("/v1/games", TeamsRouter);
 
-router.use('/v1/invites', TeamsRouter);
+router.use("/v1/invites", TeamsRouter);
+
+router.use("/v1/tournaments", TeamsRouter);
 
 // Export the base-router
 export default router;
